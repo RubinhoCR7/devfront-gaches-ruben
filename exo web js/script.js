c4682b83ec657b62086ac20ddedaf57d8801248e
Ruben <r_gaches@stu-digital-campus.fr>
2022-10-26 16:21:01 +0200

    ecriture + lecture

--- exo web js/script.js
+++ exo web js/script.js
@@ -1,4 +1,14 @@
 localStorage.setItem("ruben", "dany");
 console.log('test')
 localStorage.getItem("ruben")
-localStorage.removeItem("ruben")
+let objJson = {
+    prenom : "dany",
+    age : 30,
+    taille : 170
+}
+let objLinea = JSON.stringify(objJason);
+localStorage.setItem("obj", objLinea);
+
+let objLinea= localStorage.getItem(objJson);
+let objJason = JSON.parse(objLinea);
+alert(objJason.age) // renvoie 30
